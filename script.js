1d83906a8b71fce27a1686cd36f785e5986bcacc
Nazar_Panyushchyk <npanusik@gmail.com>
2024-01-24 18:58:46 +0200

    update javascript

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const active = document.querySelector('.active')
 support.addEventListener('click', function() {
     modal.classList.add('active')
 })
+
+const modal2 = document.querySelector('.container_modal');
+
+const support2 = document.querySelector('.button');
+console.log(support2);
+
+const actives = document.querySelector('.actives')
+support2.addEventListener('click', function() {
+    modal2.classList.add('actives')
+})
