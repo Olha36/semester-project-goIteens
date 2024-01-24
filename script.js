65f485cadb1eb9ff33d8235678d6d33a91e0a38c
Olha36 <olhakucheruk36@gmail.com>
2024-01-24 19:20:11 +0200

    fix javascript for Nazar

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ support.addEventListener('click', function() {
     modal.classList.add('active')
 })
 
-const modal2 = document.querySelector('.container_modal');
+const modal2 = document.querySelector('.container');
+console.log(modal2);
 
-const support2 = document.querySelector('.button');
+const support2 = document.querySelector('.two');
 console.log(support2);
 
-const actives = document.querySelector('.actives')
 support2.addEventListener('click', function() {
-    modal2.classList.add('actives')
+    modal2.classList.add('active')
 })
+
+// const actives = document.querySelector('.actives')
+// support2.addEventListener('click', function() {
+//     modal2.classList.add('actives')
+// })
