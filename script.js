0c758da8b01817d318f8507f878ec18e5892cb56
Olha36 <olhakucheruk36@gmail.com>
2024-01-25 12:13:01 +0200

    fix style.css + js (desktop)

--- script.js
+++ script.js
@@ -13,13 +13,19 @@ const modal2 = document.querySelector('.container_modal');
 const support2 = document.querySelector('.two');
 console.log(support2);
 
-const active2 = document.querySelector('.active2')
+const cross = document.querySelector('.container_cross')
+
+// const active2 = document.querySelector('.active2')
 support2.addEventListener('click' , function() {
-    modal2.classList.add('active2')
+    modal2.classList.add('active')
+})
+
+cross.addEventListener('click', function() {
+    modal2.classList.remove('active')
 })
 
 const closeDagger = document.querySelector('.dagger');
-console.log(support);
+console.log(closeDagger);
 
 closeDagger.addEventListener('click', function() {
     modal.classList.remove('active')
