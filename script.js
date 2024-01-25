efbbb221c504e9be7c6f14a9dfbbdab3fc95ae9c
Olha36 <olhakucheruk36@gmail.com>
2024-01-25 11:59:21 +0200

    Revert "Merge remote-tracking branch 'origin/feature/dev-version-edward' into feature/dev-version"
    This reverts commit 2a6ef66081af7c6933675c566e88cb51addc4316, reversing
    changes made to 9723f2bdd09f440acbad9cc20352291ba9e40d28.

--- script.js
+++ script.js
@@ -8,19 +8,12 @@ support.addEventListener('click', function() {
     modal.classList.add('active')
 })
 
-const closeDagger = document.querySelector('.dagger');
-console.log(support);
-
-closeDagger.addEventListener('click', function() {
-    modal.classList.remove('active')
-})
-
 const modal2 = document.querySelector('.container_modal');
 
 const support2 = document.querySelector('.two');
 console.log(support2);
 
-// const active2 = document.querySelector('.active2')
+const active2 = document.querySelector('.active2')
 support2.addEventListener('click' , function() {
-    modal2.classList.add('active')
+    modal2.classList.add('active2')
 })
